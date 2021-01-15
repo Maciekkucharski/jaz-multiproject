b42323fdfc6322e23eee817d801405e6619325f8
maciek_kucharski <m.kucha@wp.pl>
2021-01-15 11:25:27 +0100

    fix(changig har coded stuff)

--- jaz-api/src/main/java/pl/edu/pjwstk/jazapi/security/TokenAuthenticationFilter.java
+++ jaz-api/src/main/java/pl/edu/pjwstk/jazapi/security/TokenAuthenticationFilter.java
@@ -52,6 +52,8 @@ public class TokenAuthenticationFilter extends UsernamePasswordAuthenticationFil
                                             Authentication auth) throws IOException {
         String subject = auth.getPrincipal().toString();
 
+        var authorities = auth.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.toList());
+
         String token = JWT.create()
                 .withSubject(subject)
                 .withExpiresAt(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
